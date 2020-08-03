f42e10904389eea46e51e0b76a895457d43482b5
brandongatlin <brandongatlin1981@me.com>
2020-08-02 20:02:52 -0500

    added base media url as well

--- client/src/components/Pets/index.js
+++ client/src/components/Pets/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import {useQuery} from 'react-query';
 import axios from 'axios';
 
-const fetchPets = async () => {
-    const BASE_URL = 
+const BASE_URL = 
     process.env.NODE_ENV !== 'production' ? 
     'http://localhost:8000' : 
     'http://www.baytownhumanesociety.org/';
 
+const MEDIA_URL = 
+    process.env.NODE_ENV !== 'production' ? 
+    'http://localhost:8000/media' : 
+    'http://www.baytownhumanesociety.org/media';
+
+const fetchPets = async () => {
     const {data} = await axios.get(new URL(`${BASE_URL}/pets`));
     console.log(data.pets)
     return data.pets;
@@ -16,18 +21,18 @@ const fetchPets = async () => {
 const Pets = () => {
 const { data, status } = useQuery('petsQ', fetchPets);
 return(
-        <>
-        <p>PETS {status !== 'success' ? status : null}</p>
-            {
-                status === 'success' && (
-                    data.map((pet) => {
-                        return (
-                            <h1>{pet.animal_name}</h1>
-                        )
-                    })
-                )
-            }
-        </>
+    <>
+    <p>PETS {status !== 'success' ? status : null}</p>
+        {
+            status === 'success' && (
+                data.map((pet) => {
+                    return (
+                        <img alt={`Image of ${pet.animal_name}`} src={`${MEDIA_URL}/${pet.image}`}/>
+                    )
+                })
+            )
+        }
+    </>
     )
 }
 
