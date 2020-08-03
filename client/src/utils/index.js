f8114920aec709be85db464be6a8596852bf22b1
brandongatlin <brandongatlin1981@me.com>
2020-08-03 14:32:49 -0500

    added days for live span

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -8,9 +8,10 @@ const MEDIA_URL = `${BASE_URL}/media`;
 const formatBirthday = (date) => {
     // 2016-11-02T03:50:00Z
     const dif = new Date() - new Date(date);
-    const years = Math.floor(dif / 1000 / 86400 / 365);
-    const months = Math.floor((dif / 1000 / 86400) % 365 / 30);
-    const weeks = Math.floor(dif / 1000 / 86400 / 7);
+    const days = Math.floor(dif / 1000 / 86400);
+    const years = Math.floor(days / 365);
+    const months = Math.floor((days) % 365 / 30);
+    const weeks = Math.floor(days / 7);
 
     const hasAnS = (num) => {
         if (num < 2) {
@@ -36,13 +37,13 @@ const formatBirthday = (date) => {
         formattedTime += `${months} ${hasAnS(months) ? addS('Month') : 'Month'}`;
     }
 
-    if (years < 1 && months === 0) {
+    if (years === 0 && months === 0 && weeks === 0) {
+        formattedTime += `${days} ${hasAnS(days) ? addS('Day') : 'Day'}`;
+    } else if (years === 0 && months === 0) {
         formattedTime += `${weeks} ${hasAnS(weeks) ? addS('Week') : 'Week'}`;
     }
 
-
     return formattedTime + ' Old';
-
 }
 
 
